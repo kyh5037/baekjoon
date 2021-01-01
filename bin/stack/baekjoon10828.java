309cc23f35242a0713ae8b62a88113a47ed8b276
김윤환 <srar2398@naver.com>
2021-01-01 14:54:01 +0900

    baekjoon10828

---
+++ bin/stack/baekjoon10828.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class baekjoon10828 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Stack<Integer> stack = new Stack<>();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < num; i++) {
+			String[] str = br.readLine().split(" ");
+			
+			switch(str[0]) {
+			case "push":
+				stack.push(Integer.parseInt(str[1]));
+				break;
+			case "pop":
+				if(stack.empty()) {
+					bw.write("-1" + "\n");
+				} else {
+					bw.write(stack.pop()+ "\n");
+				}
+				break;
+			case "size":
+				bw.write(stack.size() + "\n");
+				break;
+			case "empty":
+				if(stack.empty()) {
+					bw.write("1" + "\n");
+				} else {
+					bw.write("0" + "\n");
+				}
+				break;
+			case "top":
+				if(stack.empty()) {
+					bw.write("-1" + "\n");
+				} else {
+					bw.write(stack.peek() + "\n");
+				}
+				break;
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+}
